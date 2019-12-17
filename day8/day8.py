893e62c5630d151c886f3bb92b855842f6a37df5
Gathaar <Potpanpit@hotmail.com>
2019-12-17 19:37:46 +0100

    Added day 8 pt 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,3 +1,6 @@
+from tkinter import *
+
+
 def render_image(raw_input, width, height):
     amount_of_layers = int(len(raw_input) / (width * height))
     image_size = width * height     # for readability
@@ -11,6 +14,17 @@ def render_image(raw_input, width, height):
         layers.append(image)
 
     return layers
+
+
+def render_visible(image):
+    visible = image[0]
+    for layer in image[1:]:
+        for i in range(len(layer)):
+            for j in range(len(layer[0])):
+                if visible[i][j] == '2' and layer[i][j] != '2':
+                    print(f'Adjusted {i} {j}')
+                    visible[i][j] = layer[i][j]
+    return visible
 
 
 def part1_solver(image):
@@ -27,11 +41,31 @@ def part1_solver(image):
     return winning_layer
 
 
+def part2_solver(image, width, height):
+    visible_to_user = render_visible(image)
+    app = Tk()
+    img_scale = 100
+    picture_width = width * img_scale
+    picture_height = height * img_scale
+    window = Canvas(app, width=picture_width, height=picture_height)
+    window.pack()
+    colours = {
+        '0': '#000000',
+        '1': '#FFFFFF',
+        '2': '#888888'
+    }
+    for i in range(height):
+        for j in range(width):
+            window.create_rectangle(j*img_scale, i*img_scale, j*img_scale+img_scale, i*img_scale+img_scale, fill=colours[visible_to_user[i][j]])
+    mainloop()
+
+
 if __name__ == '__main__':
     puzzle_input = open('input.txt', 'r').read()
     img = render_image(puzzle_input, 25, 6)
 
     part1_output_layer = part1_solver(img)
+    print(f'Part 1 height: {len(part1_output_layer)} -- width: {len(part1_output_layer[0])}')
     ones = 0
     twos = 0
     for part1_row in part1_output_layer:
@@ -39,4 +73,6 @@ if __name__ == '__main__':
         twos += part1_row.count('2')
 
     print(f'\t-- Part 1 output: {ones * twos}')
+
+    part2_solver(img, 25, 6)
 
--- day8/tests.py
+++ day8/tests.py
@@ -1,4 +1,5 @@
-from .day8 import render_image
+from .day8 import render_image, render_visible
+from tkinter import *
 import unittest
 
 
@@ -12,3 +13,13 @@ class TestMySelfies(unittest.TestCase):
         self.assertEqual(2, len(layers))        # Verify layer count
         self.assertEqual(2, len(layers[0]))     # Verify height
         self.assertEqual(['7', '8', '9'], layers[1][0])
+
+    def test_day8_1(self):
+        raw_input = '0222112222120000'
+        width = 2
+        height = 2
+
+        img = render_image(raw_input, width, height)
+        visible_layer = render_visible(img)
+
+        self.assertEqual([['0', '1'], ['1', '0']], visible_layer)
